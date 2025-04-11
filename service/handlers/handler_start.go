634293a941dc834a495a32f1cd9569451cecad7c
larkin <larkin.qubership@gmail.com>
2025-04-11 10:25:47 +0400

    Minor fixes

--- service/handlers/handler_start.go
+++ service/handlers/handler_start.go
@@ -13,13 +13,13 @@ type StartHandler struct {
 func (h StartHandler) Handle(bot *mybot.Bot, update tgbotapi.Update) error {
 	chatID := update.Message.Chat.ID
 
-	msg := "Привет! Я — бот-отхеппибёздыватель!\n" +
+	msg := "Привет! Я — бот-отхеппибёздыватель!\n\n" +
 		"Я помогу вам организовать групповые подарки на дни рождения!\n\n" +
 		"Ты можешь использовать следующие комманды:\n\n"
 	for _, command := range mybot.Commands {
 		msg += fmt.Sprintf("[/%s] — %s\n", command.Command, command.Description)
 	}
-	msg += "\n\nТак же я буду оповещать о ближайших днях рождения и управлять чатиками для обсуждения подарков! " +
+	msg += "\nТак же я буду оповещать о ближайших днях рождения и управлять чатиками для обсуждения подарков! " +
 		"Вся информация хранится в [таблице](https://docs.google.com/spreadsheets/d/1fb5ssf4Mp8HZ9aAFAOox9byQGUHstRub_5ssOdDoNro)."
 	msg += "\n\nА еще я пощу котиков `:3`"
 
